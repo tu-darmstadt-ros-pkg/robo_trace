46e179d12a82f584058cedce512d7426e39a361d
Max Schmidt <max.schmidt.11@stud.tu-darmstadt.de>
2022-04-20 23:53:56 +0200

    Added license headers to all source files.

--- robo_trace/src/processing/modules/marshalling/forward.cpp
+++ robo_trace/src/processing/modules/marshalling/forward.cpp
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2022 Fachgebiet Simulation, Systemoptimierung und Robotik, TU Darmstadt.
+ *
+ * This file is part of RoboTrace
+ * (see https://github.com/tu-darmstadt-ros-pkg/robo_trace) 
+ * and is governed by a BSD-style license 
+ * that can be found in the LICENSE file.
+ */
+
 // Base
 #include "robo_trace/processing/modules/marshalling/forward.hpp"
 // MongoCXX
---
+++ robo_trace/src/processing/translation/options.cpp
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2022 Fachgebiet Simulation, Systemoptimierung und Robotik, TU Darmstadt.
+ *
+ * This file is part of RoboTrace
+ * (see https://github.com/tu-darmstadt-ros-pkg/robo_trace) 
+ * and is governed by a BSD-style license 
+ * that can be found in the LICENSE file.
+ */
